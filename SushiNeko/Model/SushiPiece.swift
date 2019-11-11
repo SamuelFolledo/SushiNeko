eaedbd83069f9fbe72d773604a0c0d0e7cc31528
Samuel Folledo <samuelfolledo@gmail.com>
2019-11-11 15:12:00 -0800

    [add] put the project in an MVC pattern

---
+++ SushiNeko/Model/SushiPiece.swift
@@ -0,0 +1,27 @@
+//
+//  SushiPiece.swift
+//  SushiNeko
+//
+//  Created by Macbook Pro 15 on 11/11/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import SpriteKit
+
+class SushiPiece: SKSpriteNode {
+
+    /* Chopsticks objects */
+    var rightChopstick: SKSpriteNode!
+    var leftChopstick: SKSpriteNode!
+
+    /* You are required to implement this for your subclass to work */
+    override init(texture: SKTexture?, color: UIColor, size: CGSize) {
+        super.init(texture: texture, color: color, size: size)
+    }
+
+    /* You are required to implement this for your subclass to work */
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
---
+++ SushiNeko/View/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//  SushiNeko
+//
+//  Created by Macbook Pro 15 on 11/11/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+/* Tracking enum for use with character and sushi side */
+enum Side {
+    case left, right, none
+}
+
+class GameScene: SKScene {
+    
+    let kBACKGROUND: String = "background"
+    let kBASESUSHI: String = "sushiBasePiece"
+    let kLEFTCHOPSTICK: String = "leftChopstick"
+    let kRIGHTCHOPSTICK: String = "rightChopstick"
+    
+    
+}
